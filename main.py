53cededb8aefefa5a0595c43ecff1b578fa89e9b
heonilha <heonil0304@gmail.com>
2025-11-10 20:09:47 +0900

    main함수 수정
    game_framework를 통해 모드를 관리하도록 main함수를 수정

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from pico2d import *
+import game_framework
+import play_mode as start_mode
 
-open_canvas()
-character = load_image('ZAG_ani.png')
-frame = 0
-x=0
-while True:
-    clear_canvas()
-    character.clip_draw(frame * 32, 0, 32, 64, x, 300)  # 프레임 너비 32, 높이 64
-    update_canvas()
-    frame = (frame + 1) % 2
-    x += 5
-    delay(0.1)
+open_canvas(1000, 600)
+game_framework.run(start_mode)
+close_canvas()
