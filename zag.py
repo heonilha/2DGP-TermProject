78e006cbb26d5e45636f498d5e5fed737c5bcaf4
heonilha <heonil0304@gmail.com>
2025-11-12 19:52:29 +0900

    zag의 Attack 구현
    공격을 추가하면서 handle_event 로직 수정

--- zag.py
+++ zag.py
@@ -1,5 +1,5 @@
 from pico2d import *
-from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT, SDLK_UP, SDLK_DOWN
+from sdl2 import SDL_KEYDOWN, SDLK_z, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT, SDLK_UP, SDLK_DOWN
 
 import os
 import game_framework
@@ -19,6 +19,13 @@ def event_run(e):
 def event_die(e):
     return e[0] == 'DIE'
 
+def event_attack(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_z
+
+def event_timeout(e):
+    # 'TIME_OUT' 이벤트 (타이머가 다 되었을 때)
+    return e[0] == 'TIME_OUT'
+
 PIXEL_PER_METER = (10.0 / 0.5)  # 10 pixel 50 cm
 RUN_SPEED_KMPH = 20.0  # Km / Hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
@@ -30,8 +37,67 @@ IDLE_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 2
 RUN_PER_TIME = 2.5 / TIME_PER_ACTION
 
-# Die클래스 추가
-# 죽으면 화면이 어두워지면서 타이틀 화면으로 넘어감
+#Attack 클래스 추가
+class Attack:
+    def __init__(self, zag,attack_image):
+        self.zag = zag
+        self.attack_image = attack_image
+        self.attack_timer = 0.0  # 공격 지속시간 타이머
+        self.attack_duration = 0.5  # 총 공격 시간 (0.5초)
+        self.hit_monsters = []  # ◀◀◀ [중요] 이번 공격에 맞은 몬스터 목록
+
+    def enter(self,e):
+        print("Zag Attack")
+        self.attack_timer = self.attack_duration
+        self.hit_monsters.clear()
+
+        self.zag.xdir = 0
+        self.zag.ydir = 0
+    def exit(self,e):
+        pass
+
+    def do(self):
+        self.attack_timer -= game_framework.frame_time
+
+        # ◀◀◀ 공격 판정 시간 (예: 0.3초 중 0.1초~0.2초 사이)
+        # (타이머가 0.2초 ~ 0.1초 남았을 때)
+        if 0.1 < self.attack_timer < 0.2:
+            self.check_attack_collision()
+
+        # ◀◀◀ 공격 시간이 끝나면 IDLE로 복귀
+        if self.attack_timer <= 0:
+            self.zag.state_machine.handle_state_event(('TIME_OUT', None))
+
+    def draw(self):
+        #109x217 크기의 사진
+        if self.zag.face_dir == 1:
+            self.attack_image.clip_composite_draw(0, 0, 109, 217, 0, 'h', self.zag.x + 32, self.zag.y, 32, 64)
+            self.zag.image.clip_composite_draw(0, 0, 32, 64,0,'', self.zag.x, self.zag.y,32,64)
+        else:
+            self.attack_image.clip_composite_draw(0, 0, 109, 217, 0, '', self.zag.x - 32, self.zag.y, 32, 64)
+            self.zag.image.clip_composite_draw(0, 0, 32, 64, 0, 'h', self.zag.x, self.zag.y,32,64)
+    def check_attack_collision(self):
+        # 공격 이미지 크기는 폭 64, 높이 64로 가정
+
+        attack_width = 64
+        attack_height = 64
+        attack_box_x = self.zag.x + (32 if self.zag.face_dir == 1 else -32)
+        attack_box_y = self.zag.y
+        attack_bb = (attack_box_x - attack_width // 2, attack_box_y - attack_height // 2,
+                     attack_box_x + attack_width // 2, attack_box_y + attack_height // 2)
+        for monster in game_world.all_objects():
+            if monster == self.zag:
+                continue
+            if hasattr(monster, 'get_bb') and monster not in self.hit_monsters:
+                monster_bb = monster.get_bb()
+                if (attack_bb[0] < monster_bb[2] and attack_bb[2] > monster_bb[0] and
+                    attack_bb[1] < monster_bb[3] and attack_bb[3] > monster_bb[1]):
+                    # 충돌 판정이 일어났을 때 처리
+                    monster.hp -= 5
+                    self.hit_monsters.append(monster)
+                    print(f'Monster HP: {monster.hp}')
+
+
 class Die:
     def __init__(self, zag):
         self.zag = zag
@@ -108,21 +174,30 @@ class Zag:
     def __init__(self):
         self.x, self.y = 400, 300
         self.image = load_image(os.path.join(r'C:\Users\heonilha\Documents\GitHub\2DGP-TermProject\resource\Image\Character\ZAG_ani.png'))
+        self.attack_image = load_image(os.path.join(r'C:\Users\heonilha\Documents\GitHub\2DGP-TermProject\resource\Image\Character\ZAG_attack.png'))
         self.frame = 0
         self.xdir = 0
         self.ydir = 0
         self.face_dir = 1
-        self.hp = 10
+        self.hp = 100
         self.invincibleTimer = 0.0
-
+        self.keys_down = set()
+        self.key_map = {
+            SDLK_RIGHT:(1,0),
+            SDLK_LEFT:(-1,0),
+            SDLK_UP:(0,1),
+            SDLK_DOWN:(0,-1)
+        }
         self.IDLE = Idle(self)
         self.RUN = Run(self)
         self.DIE = Die(self)
+        self.ATTACK = Attack(self,self.attack_image)
         self.state_machine = StateMachine(
             self.IDLE,
             state_transitions={
-                self.IDLE: {space_down: self.IDLE, event_run: self.RUN, event_die: self.DIE},
-                self.RUN: {space_down: self.RUN, event_stop: self.IDLE, event_die: self.DIE},
+                self.IDLE: {space_down: self.IDLE, event_run: self.RUN, event_die: self.DIE,event_attack: self.ATTACK},
+                self.RUN: {space_down: self.RUN, event_stop: self.IDLE, event_die: self.DIE, event_attack: self.ATTACK},
+                self.ATTACK: {event_timeout: self.IDLE, event_die: self.DIE},
                 self.DIE: {}
             }
         )
@@ -154,39 +229,55 @@ class Zag:
             self.state_machine.draw()
 
     def handle_event(self, event):
-        if event.key in (SDLK_RIGHT, SDLK_LEFT, SDLK_UP, SDLK_DOWN):
-            cur_xdir, cur_ydir = self.xdir, self.ydir
+        # 1. ◀◀◀ [핵심 수정]
+        #    죽었거나 공격 중일 때
+        if self.state_machine.cur_state in (self.DIE, self.ATTACK):
+
+            # ◀ '이동 키'의 'KEYUP' 이벤트는 특별히 처리해서 set에서 제거
+            if event.type == SDL_KEYUP and event.key in self.key_map:
+                self.keys_down.discard(event.key)
+
+            # ◀ (필요시) 공격 중에 죽는 이벤트는 받아들일 수 있음
+            # if event_die(('INPUT', event)):
+            #    self.state_machine.handle_state_event(('DIE', None))
+
+            # ◀ 그리고 나머지 모든 이벤트(KEYDOWN, Z, Space 등)는 무시
+            return
+
+        # 2. (이하 기존 'set' 방식 로직)
+        #    살아있고, 공격 중이 아닐 때의 처리
+
+        # 3. 이동 키인지 확인
+        if event.key in self.key_map:
             if event.type == SDL_KEYDOWN:
-                if event.key == SDLK_RIGHT:
-                    self.xdir += 1
-                elif event.key == SDLK_LEFT:
-                    self.xdir -= 1
-                elif event.key == SDLK_UP:
-                    self.ydir += 1
-                elif event.key == SDLK_DOWN:
-                    self.ydir -= 1
-
+                self.keys_down.add(event.key)
             elif event.type == SDL_KEYUP:
-                if event.key == SDLK_RIGHT:
-                    self.xdir -= 1
-                elif event.key == SDLK_LEFT:
-                    self.xdir += 1
-                elif event.key == SDLK_UP:
-                    self.ydir -= 1
-                elif event.key == SDLK_DOWN:
-                    self.ydir += 1
-            if cur_xdir != self.xdir or cur_ydir != self.ydir:
-                # 가로 방향이 변경되어 0이 아닌 경우 즉시 face_dir 업데이트
-                if self.xdir != 0:
-                    self.face_dir = self.xdir
+                self.keys_down.discard(event.key)
 
-                if self.xdir == 0 and self.ydir == 0:
-                    self.state_machine.handle_state_event(('STOP', self.face_dir))
-                else:
-                    self.state_machine.handle_state_event(('RUN', None))
+        # 4. 이동 키가 아니라면 (공격, 스페이스바 등)
         else:
             self.state_machine.handle_state_event(('INPUT', event))
 
+        # 5. set을 기반으로 xdir/ydir을 매번 새로 계산
+        cur_xdir, cur_ydir = self.xdir, self.ydir
+
+        self.xdir = 0
+        self.ydir = 0
+        for key in self.keys_down:
+            if key in self.key_map:
+                dx, dy = self.key_map[key]
+                self.xdir += dx
+                self.ydir += dy
+
+        # 6. 방향이 변경되었는지 확인하고 상태 전이
+        if cur_xdir != self.xdir or cur_ydir != self.ydir:
+            if self.xdir != 0:
+                self.face_dir = self.xdir
+
+            if self.xdir == 0 and self.ydir == 0:
+                self.state_machine.handle_state_event(('STOP', self.face_dir))
+            else:
+                self.state_machine.handle_state_event(('RUN', None))
     def get_bb(self):
         return self.x - 16, self.y - 32, self.x + 16, self.y + 32
 
