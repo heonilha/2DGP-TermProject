f3df35abf1176d9e1569268a5d59a9a5b87fa75c
heonilha <heonil0304@gmail.com>
2025-11-12 18:27:30 +0900

    타이틀 화면을 시작화면으로 변경

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pico2d import *
 import game_framework
-import play_mode as start_mode
+import title_mode as start_mode
 
-open_canvas(1000, 600)
+open_canvas(1200, 700)
 game_framework.run(start_mode)
 close_canvas()
