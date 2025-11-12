2c284351bcabfda1911b3c76c247c6599050c76c
heonilha <heonil0304@gmail.com>
2025-11-12 18:26:57 +0900

    타이틀 화면 구현

---
+++ title_mode.py
@@ -0,0 +1,34 @@
+from pico2d import *
+import game_framework
+import play_mode
+image = None
+
+def init():
+    global image
+    base_dir = os.path.dirname(__file__)
+    image_path = os.path.join(base_dir, 'resource', 'Image', 'GUI', 'UI_TITLE_TITLE.jpg')
+    if not os.path.exists(image_path):
+        raise FileNotFoundError(f"Image not found: `{image_path}`")
+    image = load_image(image_path)
+def finish():
+    global image
+    del image
+def handle_events():
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
+            game_framework.change_mode(play_mode)
+def draw():
+    clear_canvas()
+    image.draw(600,350, 1200,700)
+    update_canvas()
+def update():
+    pass
+def pause():
+    pass
+def resume():
+    pass
