7963db28cbad151e6117a2fe69ba9a880aedf7bd
heonilha <heonil0304@gmail.com>
2025-11-22 15:04:38 +0900

    UI화면을 위한 클래스 추가

---
+++ ui.py
@@ -0,0 +1,30 @@
+from pico2d import *
+import os
+from zag import Zag
+BASE_DIR = os.path.dirname(__file__)
+
+class GameUI:
+    def __init__(self):
+        self.hp_potion_image = load_image(os.path.join(BASE_DIR, 'resource', 'Image', 'GUI','Item', 'hp_potion.png'))
+        self.mp_potion_image = load_image(os.path.join(BASE_DIR, 'resource', 'Image', 'GUI','Item', 'mp_potion.png'))
+        self.gold_image = load_image(os.path.join(BASE_DIR, 'resource', 'Image', 'GUI','Item', 'bar_coin.png'))
+        self.font = load_font('ENCR10B.TTF', 30)
+
+    def draw(self, player):
+        # --- HP 물약 그리기 (화면 왼쪽 아래) ---
+        # 이미지 그리기 (x=50, y=100)
+        self.hp_potion_image.draw(50, 100)
+
+        # 개수 텍스트 그리기 (Zag 인벤토리에서 가져옴)
+        hp_count = player.hp_potions
+        self.font.draw(100, 100, f'x {hp_count}', (255, 255, 255))  # 흰색 글씨
+
+        # --- MP 물약 그리기 (HP 물약 아래) ---
+        self.mp_potion_image.draw(50, 50)
+
+        mp_count = player.mp_potions
+        self.font.draw(100, 50, f'x {mp_count}', (255, 255, 255))
+        # --- 골드 그리기 (화면 오른쪽 위) ---
+        self.gold_image.draw(1050, 580)
+        gold_count = player.gold
+        self.font.draw(1100, 580, f'x {gold_count}', (255, 255, 0))  # 노란색 글씨
