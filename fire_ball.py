359bc2cb7f8df95512eee5622a6f0e8b0da7e081
heonilha <heonil0304@gmail.com>
2025-11-29 10:36:50 +0900

    Refactor projectile to component-based object

--- fire_ball.py
+++ fire_ball.py
@@ -1,23 +1,25 @@
-from projectile import Projectile
-import game_framework
-from pico2d import *
+import math
 import os
+
+from pico2d import load_image
+
+from projectile import Projectile
 
 base_dir = os.path.dirname(__file__)
 image_path = os.path.join(base_dir, 'resource', 'Image', 'Character', 'Fire', 'Fire01_1.png')
 
+
 class FireBall(Projectile):
     image = None
-    def __init__(self, x, y, direction_x,direction_y):
+
+    def __init__(self, x, y, direction):
         if FireBall.image is None:
             FireBall.image = load_image(image_path)
+
         damage = 5
-        speed=1000
-        direction = (direction_x, direction_y)
-        super().__init__(x, y, direction,speed, damage,30,30)
+        speed = 1000
 
-    def draw(self):
-        self.image.draw(self.x, self.y, 30, 30)
+        norm = math.hypot(direction[0], direction[1])
+        dir_x, dir_y = direction if norm == 0 else (direction[0] / norm, direction[1] / norm)
 
-    def on_hit(self, target):
-        super().on_hit(target)
+        super().__init__(x, y, (dir_x, dir_y), speed, damage, 30, 30, FireBall.image)
