60fcac19654bb84b4be1d45d13adacd549ef3b9c
heonilha <heonil0304@gmail.com>
2025-11-29 21:09:48 +0900

    Use combat component for slime damage

--- components/component_projectile.py
+++ components/component_projectile.py
@@ -1,5 +1,6 @@
 import game_world
 from components.component_base import Component
+from components.component_combat import CombatComponent
 
 
 class ProjectileComponent(Component):
@@ -8,6 +9,9 @@ class ProjectileComponent(Component):
         self.damage = damage
 
     def on_hit(self, target):
-        if hasattr(target, 'take_damage'):
+        combat = target.get(CombatComponent) if hasattr(target, 'get') else None
+        if combat:
+            combat.take_damage(self.damage)
+        elif hasattr(target, 'take_damage'):
             target.take_damage(self.damage)
         game_world.remove_object(self.owner)
