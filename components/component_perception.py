7221f5f5e0094963937d4edde676f3c4acedd2cb
heonilha <heonil0304@gmail.com>
2025-12-04 20:29:16 +0900

    Remove redundant slime distance helper

--- components/component_perception.py
+++ components/component_perception.py
@@ -2,33 +2,21 @@ from components.component_base import Component
 
 
 class PerceptionComponent(Component):
-    def __init__(self, detection_range, target_getter=None):
+    def __init__(self, owner=None):
         super().__init__()
-        self.detection_range = detection_range
-        self.detection_range_sq = detection_range * detection_range
-        self.target_getter = target_getter
-        self.override_target = None
-
-    def set_target(self, target):
-        self.override_target = target
-
-    def get_target(self):
-        if self.override_target is not None:
-            return self.override_target
-        if callable(self.target_getter):
-            return self.target_getter()
-        return None
+        if owner is not None:
+            self.owner = owner
+        self.target = None
 
     def distance_sq_to_target(self):
-        target = self.get_target()
-        if not target:
-            return None
-        dx = getattr(self.owner, "x", 0) - getattr(target, "x", 0)
-        dy = getattr(self.owner, "y", 0) - getattr(target, "y", 0)
+        if not self.target or not getattr(self.target, "transform", None):
+            return 999_999_999
+        if not getattr(self.owner, "transform", None):
+            return 999_999_999
+
+        dx = self.owner.transform.x - self.target.transform.x
+        dy = self.owner.transform.y - self.target.transform.y
         return dx * dx + dy * dy
 
-    def is_target_in_range(self):
-        distance_sq = self.distance_sq_to_target()
-        if distance_sq is None:
-            return False
-        return distance_sq <= self.detection_range_sq
+    def is_in_range(self, range_):
+        return self.distance_sq_to_target() <= range_ * range_
