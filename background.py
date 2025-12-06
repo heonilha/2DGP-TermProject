fdf48708e0b11560ac771f765f6d09e79d488220
heonilha <heonil0304@gmail.com>
2025-12-06 12:09:52 +0900

    Update camera movement and background rendering (#35)

--- background.py
+++ background.py
@@ -14,18 +14,26 @@ class Background:
         self.image = Background._cache[filename]
 
     def draw(self):
-        self.image.draw(get_canvas_width()//2, get_canvas_height()//2, get_canvas_width(), get_canvas_height())
+        self.draw_with_camera(None)
 
     def draw_with_camera(self, camera):
         if not self.image:
             return
 
+        canvas_w = get_canvas_width()
+        canvas_h = get_canvas_height()
+
         offset_x = camera.x if camera else 0
         offset_y = camera.y if camera else 0
 
-        center_x = (self.image.w // 2) - offset_x
-        center_y = (self.image.h // 2) - offset_y
-        self.image.draw(center_x, center_y, get_canvas_width(), get_canvas_height())
+        max_x = max(0, self.image.w - canvas_w)
+        max_y = max(0, self.image.h - canvas_h)
+
+        source_x = clamp(0, offset_x, max_x)
+        source_y = clamp(0, offset_y, max_y)
+
+        self.image.clip_draw(int(source_x), int(source_y), canvas_w, canvas_h,
+                             canvas_w // 2, canvas_h // 2, canvas_w, canvas_h)
 
     def update(self):
         pass
