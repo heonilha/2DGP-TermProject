fa3630296d80b41f806a50efc4dddd718102cae3
heonilha <heonil0304@gmail.com>
2025-12-06 12:21:28 +0900

    Refactor background for vertical wrap

--- background.py
+++ background.py
@@ -14,18 +14,36 @@ class Background:
         self.image = Background._cache[filename]
 
     def draw(self):
-        self.image.draw(get_canvas_width()//2, get_canvas_height()//2, get_canvas_width(), get_canvas_height())
+        self.draw_with_camera(game_world.camera)
 
     def draw_with_camera(self, camera):
         if not self.image:
             return
 
-        offset_x = camera.x if camera else 0
-        offset_y = camera.y if camera else 0
+        canvas_w = get_canvas_width()
+        canvas_h = get_canvas_height()
 
-        center_x = (self.image.w // 2) - offset_x
-        center_y = (self.image.h // 2) - offset_y
-        self.image.draw(center_x, center_y, get_canvas_width(), get_canvas_height())
+        img_h = self.image.h
+        img_w = self.image.w
+
+        offset = int(camera.y % img_h) if camera else 0
+
+        upper_h = min(img_h - offset, canvas_h)
+        lower_h = max(0, canvas_h - upper_h)
+
+        center_x = canvas_w // 2
+
+        if upper_h > 0:
+            upper_center_y = canvas_h - upper_h / 2
+            self.image.clip_draw(0, offset, img_w, upper_h,
+                                 center_x, upper_center_y,
+                                 canvas_w, upper_h)
+
+        if lower_h > 0:
+            lower_center_y = lower_h / 2
+            self.image.clip_draw(0, 0, img_w, lower_h,
+                                 center_x, lower_center_y,
+                                 canvas_w, lower_h)
 
     def update(self):
         pass
