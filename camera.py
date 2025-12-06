1fa61d064d2c8aa94d6c0a0036a26e9481b811ae
heonilha <heonil0304@gmail.com>
2025-12-06 19:07:27 +0900

    Adjust movement bounds to background size (#38)

--- camera.py
+++ camera.py
@@ -5,8 +5,10 @@ class Camera:
     def __init__(self):
         self.x = 0
         self.y = 0
+        self.world_width = get_canvas_width()
+        self.world_height = get_canvas_height()
 
     def update(self, target):
         h = get_canvas_height()
 
-        self.y = clamp(0,target.y - h // 4, 1800)
+        self.y = clamp(0, target.y - h // 4, self.world_height)
