c4f4ccd241f019081c96bb233bc140f946a175d3
heonilha <heonil0304@gmail.com>
2025-12-07 12:38:53 +0900

    Draw player HP/MP bars in UI (#48)

--- zag.py
+++ zag.py
@@ -170,7 +170,8 @@ class Zag(GameObject):
             )
         )
 
-        self.mp = 100
+        self.max_mp = 100
+        self._mp = self.max_mp
         self.attack_cooldown = 1.0
         self.attack_cooldown_timer = 0.0
         self.hp_potions = 3
@@ -265,6 +266,14 @@ class Zag(GameObject):
     @invincibleTimer.setter
     def invincibleTimer(self, value):
         self.combat.invincible_timer = max(0.0, value)
+
+    @property
+    def mp(self):
+        return self._mp
+
+    @mp.setter
+    def mp(self, value):
+        self._mp = max(0, min(self.max_mp, value))
 
     def update(self):
         if self.hp <= 0 and self.state_machine.cur_state != self.DIE:
